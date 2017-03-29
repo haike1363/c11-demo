46bbed848374f7cf400fb34dc233e2e1a7f1bcee
muzhu.cy <muzhu.cy@alibaba-inc.com>
2017-03-29 22:41:54 +0800

    add findpackages

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include "sub.h"
 #include "add.h"
+#include "gflags/gflags.h"
 
 int main(int argc, char **argv) {
+    gflags::AllowCommandLineReparsing();
+	gflags::ParseCommandLineFlags(&argc, &argv, true);
     std::cout << "hello 1+1=" << add(1, 1) << std::endl;
 	std::cout << "hello 1-1=" << sub(1, 1) << std::endl;
     return 0;
